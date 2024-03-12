74d4e256acb016e2333ca52db2bf98312de50942
Jendosek <bala01081977@gmail.com>
2024-03-12 22:17:28 +0200

    rewrite text with windows.h library

--- rep1/rep1.cpp
+++ rep1/rep1.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     cout << "Тут мав би бути текст першого завдання, але з попередніх домашніх в мене тільки скріни((";
 }
 
